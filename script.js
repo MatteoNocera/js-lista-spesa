ee00e2092db0cd8620d8a42e21154a6d0f99a22d
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-30 17:47:21 +0200

    complete ex

--- script.js
+++ script.js
@@ -8,3 +8,50 @@ Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione
 Tutto qua! :faccia_che_festeggia:
 
 */
+
+// Data una lista della spesa
+
+// 'pomodori', 'mozzarella', 'avocado', 'cipolle', 'carote', 'latte'
+
+const buyList = [];
+
+let insert = true;
+
+let product = '';
+
+let ulElement = document.createElement('ul');
+
+document.querySelector('body').appendChild(ulElement)
+
+let liElement;
+
+
+//ulElement.innerHTML('La tua lista della spesa è :');
+document.querySelector('ul').innerHTML = 'La tua lista della spesa è :'
+
+while (insert) {
+
+
+    product = prompt('cosa serve in casa?');
+    buyList.push(product);
+    console.log(product);
+
+    
+    let liElement = document.createElement('li');
+    
+    ulElement.appendChild(liElement);
+
+    liElement.innerHTML = product
+
+    if (confirm('Ti serve altro?') != true) {
+        insert = false;
+      }
+
+    
+}
+
+console.log(buyList);
+
+
+
+
